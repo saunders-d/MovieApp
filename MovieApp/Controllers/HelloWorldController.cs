b9d94fb840d9374e68f4cee18d4023970cfaca8e
Dakota Saunders <dakotabsaunders@gmail.com>
2022-01-26 19:49:26 -0500

    Create hello world controller

---
+++ MovieApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApp.Controllers;
+
+public class HelloWorldController : Controller
+{
+    // GET
+    public string Index()
+    {
+        return "This is the default action";
+    }
+    
+    public string Welcome(string name, int id)
+    {
+        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {id}");
+    }
+}
